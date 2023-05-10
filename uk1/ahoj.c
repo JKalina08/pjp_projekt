1d6d5a3a43a533def7be3300bf1a02386e187e1c
krummhaus <jiri.kalina.jr@gmail.com>
2023-05-10 18:27:05 +0200

    local git init

---
+++ uk1/ahoj.c
@@ -0,0 +1,9 @@
+/* xPJP ukol 1
+ * kalina08@student.vspj.cz
+*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello xPJP\n");
+    return 0;
+}
---
+++ uk5/test.c
@@ -0,0 +1,8 @@
+int main(void)
+       {    
+            int x;
+            x = 19;
+            // 
+            x = x * x;
+            return 0;
+       }
